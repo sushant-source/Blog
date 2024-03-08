59c21eb1fdab282549b345f79c9cc9febbaaab40
Digital_Sushant <sushantsingh407@gmail.com>
2024-03-08 19:26:47 +0530

    PostEntity.class created and mapping done

--- src/main/java/com/sushant/entites/User.java
+++ src/main/java/com/sushant/entites/User.java
@@ -1,10 +1,16 @@
 package com.sushant.entites;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,7 +24,7 @@ import lombok.Setter;
 public class User {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	
 	@Column(name= "user_name", nullable = false, length = 100)
@@ -26,4 +32,7 @@ public class User {
 	private String email;
 	private String password;
 	private String about;
+	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Post> posts = new ArrayList<>();
 }
